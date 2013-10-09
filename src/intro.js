68c2418f2f4e62c91fd5d24909040d0ac648200a
Stephen Bunch <bunchclone@gmail.com>
2013-10-09 10:38:12 -0700

    Added bb short syntax.

--- src/intro.js
+++ src/intro.js
@@ -2,7 +2,7 @@
 
 // Until browsers can agree on what "strict" means, we won't use it.
 // "use strict";
-var bb = window.bubbles = {};
+var bb = window.bb = window.bubbles = {};
 
 // This project contains modified snippets from jQuery.
 // Copyright 2005, 2013 jQuery Foundation, Inc. and other contributors
